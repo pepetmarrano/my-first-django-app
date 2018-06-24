1376589954335aea6804436420bc71af27f08c4f
pepet <pepet.marrano@gmail.com>
2018-06-24 19:58:22 +0200

    Django app, 1er commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hp1\desktop\arxius jordi\big_data\012-django\01-tutorial\01-part1\ve_proj1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
